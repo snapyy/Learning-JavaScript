9894936b6bdf09811ea2104677b6884391ef680e
snapyy <116169769+snapyy@users.noreply.github.com>
2023-08-24 23:19:04 +0530

    Update script.js

--- learnings/PROJECTS/PROJECT-1/PART-2/script.js
+++ learnings/PROJECTS/PROJECT-1/PART-2/script.js
@@ -5,7 +5,7 @@ const ctx=canvas.getContext('2d');
 const CANVAS_WIDTH=canvas.width=800;
 const CANVAS_HEIGHT=canvas.height=700;
 
-let gameSpeed =5; // This variable will be used controlling speed.
+let gameSpeed =10; // This variable will be used controlling speed.
 
 // importing images into our canvas project .
 const backgroundLayer1=new Image(); 
@@ -19,25 +19,71 @@ backgroundLayer4.src='/backgroundLayers/layer-4.png';
 const backgroundLayer5=new Image();
 backgroundLayer5.src='/backgroundLayers/layer-5.png';
 
- let x=0; // This x variable which will server as horizontal position for one of my background images.
- let x2=2400; // This is also a same image which will use to loop the background endlessly.
-// Below code content all the code i need to draw my backgrounds
+// This is one way to do parallax
+
+//  let x=0; // This x variable which will server as horizontal position for one of my background images.
+//  let x2=2400; // This is also a same image which will use to loop the background endlessly.
+// // Below code content all the code i need to draw my backgrounds
+
+
+// function animate(){
+//     // In the below line , we are clearing the image by removing previous frames then we have to use below line.
+//     // it expects four argument to determine what part of canvas i want to delete. In our case i want clear entire canvas.
+//     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+// //drawing from coordinate 0 0 so from the top left corner of canvas.
+// //    ctx.drawImage(backgroundLayer4,x,0);
+// //    ctx.drawImage(backgroundLayer4,x2,0);
+
+
+// //    x--; // Using this x variable every time animation loop runs i decrease x by 1 . this will make our image move to the left in a negative direction on horizontal x-axis.
+// // if(x<-2400)x=2400+x2-gameSpeed; // if the background is at the end repeat it again from starting point. size of image is 2400 pixels wide. And -gameSpeed will control the gap between two same images.
+// // else x-=gameSpeed; // background will run as per the speed variable which is declare globally.
+// // if(x2<-2400) x2=2400+x-gameSpeed; // -gameSpeed also here , we are going to cut gameSpeed to control the gap between two images. +x will  will know the position the position of first image and then , it will calculate and apply the 2nd image. In the upper if statement it is vice versa.
+// // else x2-=gameSpeed; 
+//    //calling built in function which is calling this function , so this way it will call in loop.
+//     requestAnimationFrame(animate);
+// };
+// animate();
+
+
+// This another way to do parallax
+
+// In the below class we are going to use same properties with values to repeat background in loop.
+class Layer{
+constructor(image,speedModifier){ // speedModifier will set how fast my background Image.
+    this.x=0; // set x property in this particular property 0
+    this.y=0;
+    this.width=2400;
+    this.height=700;
+    this.x2=this.width;
+    this.image=image;
+    this.speedModifier=speedModifier;
+    this.speed=gameSpeed*this.speedModifier; // Doing this will allow me to passs different speed modifier value for each of my five layer. and that way each layer will move at a different speed but it will still be tied to my global  game speed.
+}
+// Below line function will move layers horizontally by changing their disk dot x and this dot x2 and it will reset them when the layers move the screen.
+update(){
+    this.speed=gameSpeed*this.speedModifier;
+    if(this.x<=-this.width){
+        this.x=this.width+this.x2-this.speed; // This line will make sure that there is no gap in the background .
+    }
+    if(this.x2<=-this.width){
+        this.x2 =this.width+this.x-this.speed;
+    }
+    this.x=Math.floor(this.x-this.speed); // To make background layer to move left i will wrap it in method floor make sure we have to decimal points in there.
+    this.x2=Math.floor(this.x2-this.speed); // Same thing as above.
+ }
+// Below line function job will be to take information about this layer object and to draw it on the canvas every time update method runs to change horizontal x position draw will run again to redraw the image after new position to make sure my game speed is dynamic and always reacting to the current value of my global gameSpeed variable .
+draw(){
+ctx.drawImage(this.image,this.x,this.y,this.width,this.height);
+ctx.drawImage(this.image,this.x2,this.y,this.width,this.height); // Drawing same image in horizontal coordinate.
+}
+}
+const layer4=new Layer(backgroundLayer4,0.5); // Calling the constructor and giving argument to perform opearation which we declared in the class.
 
 function animate(){
-    // In the below line , we are clearing the image by removing previous frames then we have to use below line.
-    // it expects four argument to determine what part of canvas i want to delete. In our case i want clear entire canvas.
     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-//drawing from coordinate 0 0 so from the top left corner of canvas.
-   ctx.drawImage(backgroundLayer4,x,0);
-   ctx.drawImage(backgroundLayer4,x2,0);
-
-
-//    x--; // Using this x variable every time animation loop runs i decrease x by 1 . this will make our image move to the left in a negative direction on horizontal x-axis.
-if(x<-2400)x=2400+x2-gameSpeed; // if the background is at the end repeat it again from starting point. size of image is 2400 pixels wide. And -gameSpeed will control the gap between two same images.
-else x-=gameSpeed; // background will run as per the speed variable which is declare globally.
-if(x2<-2400) x2=2400+x-gameSpeed; // -gameSpeed also here , we are going to cut gameSpeed to control the gap between two images. +x will  will know the position the position of first image and then , it will calculate and apply the 2nd image. In the upper if statement it is vice versa.
-else x2-=gameSpeed; 
-   //calling built in function which is calling this function , so this way it will call in loop.
+    layer4.update();
+    layer4.draw();
     requestAnimationFrame(animate);
 };
 animate();
