29a1dd034172e0c3ebe64fa2ea6dad31b032e706
snapyy <116169769+snapyy@users.noreply.github.com>
2023-08-27 19:47:03 +0530

    script.js

---
+++ learnings/PROJECTS/PROJECT-1/PART-3/script.js
@@ -0,0 +1,3 @@
+/** @type {HTMLCanvasElement}*/ // In this line ,this way i tell vs code this is a canvas project and it will suggest built-in html canvas methods for me.
+const canvas=document.getElementById('canvas1');
+const ctx=canvas.getContext('2d');
