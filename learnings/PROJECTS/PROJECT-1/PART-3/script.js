d8e4cf70a5379c939522c0c41ca7483bc4655f40
snapyy <116169769+snapyy@users.noreply.github.com>
2023-08-29 21:06:27 +0530

    Update script.js

--- learnings/PROJECTS/PROJECT-1/PART-3/script.js
+++ learnings/PROJECTS/PROJECT-1/PART-3/script.js
@@ -3,27 +3,33 @@ const canvas=document.getElementById('canvas1');
 const ctx=canvas.getContext('2d');
 CANVAS_WIDTH=canvas.width=500;
 CANVAS_HEIGHT=canvas.height=1000;
-
-
+const numberOfEnemeies=100;
+const enemeiesArray=[];
 // In the below class we are going to create all the enemys .
 class Enemy{
     constructor(){
-        this.x=10;
-        this.y=50;
+        this.x=Math.random()*canvas.width; // This line will create random x- axis coordinate when ever i create a new enemy it can apply.
+        this.y=Math.random()*canvas.height; // This line will create random y- axis coordinate  same goes here . 
         this.width=100;
         this.height=100;
+        this.speed=Math.random()*4-2; // Random number between minus 2 and plus 2 , because some of enemys have to go in the left side and some of enemys have to go in right side , for right there is normal number and for left there is negative number.
     }
     // In the below line , this method will update coordindate of my objects it will handle position calculation and movement
     update(){
-       this.x++; // For every frame increase by one and also increase y by one.
-       this.y++;
+       this.x+=this.speed; // For every frame increase by one and also increase y by one.
+       this.y+=this.speed;
     }
     // In the below line code , it will animate for every enemy easily without repeating any code . 
     draw(){
-        ctx.fillRect(this.x,this.y,this.width,this.height);
+        ctx.strokeRect(this.x,this.y,this.width,this.height);
     }
 };
-const enemy1=new Enemy(); // The new keyword will simply look at that class name in the above code it will enter its constructor ,it will create one new blank javaScript object and it will assign it values and properties based on a blueprint inside the constructor .
+// const enemy1=new Enemy(); // The new keyword will simply look at that class name in the above code it will enter its constructor ,it will create one new blank javaScript object and it will assign it values and properties based on a blueprint inside the constructor .
+
+// In the below line, We are going to create lots enemeies using for loop. 
+for(let i=0;i<numberOfEnemeies;i++){
+enemeiesArray.push(new Enemy());
+}
 
 // In the below line here is another enemy
 const enemy2=new Enemy();
@@ -36,15 +42,21 @@ function animate(){
     // enemy1.y++; // it is increasing in vertical position  by 1 for every frame.
 
     // Here i am calling the method instead of writing upper code lins for every enemy.
-    enemy1.update();
+    // enemy1.update();
 
     // Instead of using below  which on the line number 46 ,we can draw it for every enemy. just by calling method draw().
-    enemy1.draw();
+    // enemy1.draw();
 // Below code for enemy two
 // enemy2.x+=0.5;
 // enemy2.y+=0.5;
-    ctx.fillRect(enemy1.x,enemy1.y,enemy1.width,enemy1.height);  // Drawing the rectangle for enemy one
+    // ctx.fillRect(enemy1.x,enemy1.y,enemy1.width,enemy1.height);  // Drawing the rectangle for enemy one
     // ctx.fillRect(enemy2.x,enemy2.y,enemy2.width,enemy2.height); // Drwaing the rectangle for enemy two
+
+    // In the below line , we are going to apply draw() and update() method through for each loop for all enemys
+    enemeiesArray.forEach(enemy=>{
+        enemy.update();
+        enemy.draw();
+    });
     requestAnimationFrame(animate); // By using this line , we are looping this function.
 }
 animate();
