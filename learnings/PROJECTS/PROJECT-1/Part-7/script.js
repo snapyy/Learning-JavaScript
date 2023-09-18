96cd7ac1f2699c082f5ae5b963fd57a06aa74ff4
snapyy <116169769+snapyy@users.noreply.github.com>
2023-09-18 23:24:57 +0530

    script.js

--- learnings/PROJECTS/PROJECT-1/Part-7/script.js
+++ learnings/PROJECTS/PROJECT-1/Part-7/script.js
@@ -6,7 +6,8 @@ window.addEventListener('load', function () {
     const ctx = canvas.getContext('2d');
     canvas.width = 800;
     canvas.height = 720;
-
+    let enemies =[];
+    let score=0;
     // In the below class , input handler class will apply event listeners to the game and it will keep track of all keys that are currently pressed down .
     class InputHandler {
         constructor() {
@@ -40,25 +41,42 @@ window.addEventListener('load', function () {
             this.x = 0;
             this.y = this.gameHeight - this.height;
             this.image = document.getElementById('playerImage');
+            this.frameX=0;
+            this.maxFrame=8;
             this.frameX = 0;
             this.frameY = 0;
+            this.fps=20;    
+            this.frameTimer=0;
+            this.frameInterval=1000/this.fps;
             this.speed = 0;
             this.vy=0;
             this.weight=1;
         }
         draw(context){
-            context.fillStyle = 'white';
-            context.fillRect(this.x, this.y, this.width, this.height);
+            context.strokeStyle='white';
+            context.strokeRect(this.x,this.y,this.width,this.height);
+            // context.fillStyle = 'white';
+            // context.fillRect(this.x, this.y, this.width, this.height);
             context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
         }
-        update(input) {
+        update(input,deltaTime) {
+            // sprite animation 
+            if(this.frameTimer>this.frameInterval){
+                if(this.frameX>=this.maxFrame) this.frameX=0;
+                else this.frameX++;
+                this.frameTimer=0;  
+            }else{
+                this.frameTimer+=deltaTime;
+            }
+           
+            // controls
             if (input.keys.indexOf('ArrowRight') > -1) {
                 this.speed = 5;
             } else if (input.keys.indexOf('ArrowLeft')>-1) {
                 this.speed = -5;
             }
-              else if(input.keys.indexOf('ArrowUp')>-1){
-                this.vy-=10;
+              else if(input.keys.indexOf('ArrowUp')>-1 && this.onGround()){
+                this.vy-=32;
               }  
              else {
                 this.speed = 0;
@@ -73,8 +91,13 @@ window.addEventListener('load', function () {
             this.y+=this.vy;
             if(!this.onGround()){
                 this.vy+=this.weight;
+                this.frameY=1;
+                this.maxFrame=5;
+                this.frameY=1;
             }else {
                 this.vy=0;
+                this.maxFrame=8;
+                this.frameY=0;
             }
             if(this.y>this.gameHeight-this.height) this.y=this.gameHeight-this.height
         }
@@ -83,24 +106,104 @@ window.addEventListener('load', function () {
         }
     }
     class Background {
+        constructor(gameWidth,gameHeight){
+            this.gameWidth=gameWidth;
+            this.gameHeight=gameHeight;
+            this.image=document.getElementById('backgroundImage');
+            this.x=0;
+            this.y=0;
+            this.width=2400;
+            this.height720;
+            this.speed=7;
+        }
+        draw(context){
+            context.drawImage(this.image,this.x,this.y,this.width,this.height);
+            context.drawImage(this.image,this.x+this.width-this.speed,this.y,this.width,this.height);
 
+        }
+        update(){
+            this.x-=this.speed;
+            if(this.x<0-this.width)this.x=0;
+        }
     }
     class Enemy {
-
+        constructor(gameWidth,gameHeight){
+            this.gameWidth=gameWidth;
+            this.gameHeight=gameHeight;
+            this.width=160;
+            this.height=119;
+            this.image=document.getElementById('enemyImage');
+            this.x=this.gameWidth;
+            this.y=this.gameHeight-this.height;
+            this.maxFrame=5;
+            this.fps=20;    
+            this.frameTimer=0;
+            this.frameInterval=1000/this.fps;
+            this.speed=8;
+            this.markedForDeletion=false;
+        }
+        draw(context){
+            context.strokeStyle='white';
+            context.strokeRect(this.x,this.y,this.width,this.height);
+            context.drawImage(this.image,this.frameX*this.width,0,this.width,this.height,this.x,this.y,this.width,this.height);
+        }
+        update(deltaTime){
+           
+            if(this.frameTimer>this.frameInterval){
+                if(this.frameX>=this.maxFrame) this.frameX=0;
+            else this.frameX++;
+            this.frameTimer=0;
+            }else{
+                this.frameTimer+=deltaTime;
+            }
+            this.x-=this.speed;
+            if(this.x<0-this.width){
+                this.markedForDeletion=true;
+                score++;
+            }
+        }
     }
-    function handEnemy() {
-
+    function handleEnemies(deltaTime) {
+         if(enemyTimer>enemyInterval +randomEnemyInterval){
+    enemies.push(new Enemy(canvas.width,canvas.height))
+    console.log(enemies);
+    randomEnemyInterval=Math.random()*1000+500;
+            enemyTimer=0;
+        }else{
+            enemyTimer+=deltaTime;
+        }
+        enemies.forEach(enemy=>{
+            enemy.draw(ctx);
+            enemy.update(deltaTime);
+        });
+        enemies =enemies.filter(enemy=> !enemy.markedForDeletion);
     }
-    function displayStatusText() {
-
+    function displayStatusText(context) {
+        context.font='40px Helvectica';
+        context.fillStyle='black';
+        context.fillText('Score: '+score,20,50);
+        context.fillStyle='white';
+        context.fillText('Score: '+score,20,52);
     }
     const input = new InputHandler();
     const player = new Player(canvas.width, canvas.height);
-    function animate() {
+    const background= new Background(canvas.width,canvas.height);
+    
+    let lastTime=0;
+    let enemyTimer=0;
+    let enemyInterval=2000;
+    let randomEnemyInterval=Math.random()*1000+500;
+    function animate(timeStamp) {
+        const deltaTime=timeStamp-lastTime;
+        lastTime=timeStamp;     
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        background.draw(ctx);
+        // background.update();
         player.draw(ctx);
-        player.update(input);
+        player.update(input,deltaTime);
+        handleEnemies(deltaTime);     
+        displayStatusText(ctx);
         requestAnimationFrame(animate);
     }
-    animate();
+    animate(0);
 });
