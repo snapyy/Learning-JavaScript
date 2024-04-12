50825d7a16c040c8f9b14401a2224232cb722934
snapyy <210510113165@paruluniversity.ac.in>
2024-04-12 19:40:20 +0530

    Add files via upload

---
+++ practice-1/javascript-starter-file/0_variables.js
@@ -0,0 +1,31 @@
+//* ====================================
+//* Values and Variable in JavaScript
+//* ====================================
+
+//* In JavaScript, values and variables are fundamental concepts that form the basis of programming.
+
+//* Values:  A value is a piece of information that a program can work with. It can be a number, text, true/false, or more complex data.
+
+//* Variables: A variable is a container that holds a value. It has a name and can be used to store and manipulate data in a program.
+
+// console.log("Welcome back to another video 😁");
+
+// var myAge = 30;
+// console.log(myAge);
+
+//! Let's test
+
+// var my_firstName = "John";
+// console.log(my_firstName);
+
+// var _myLastName$ = "Doe";
+// console.log(_myLastName$);
+
+// Invalid
+// var 123myAge = 25;
+
+// var $cityName = "New York";
+// console.log($cityName);
+
+// Invalid
+// var my@Email = "john@example.com";
