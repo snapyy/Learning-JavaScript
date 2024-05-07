bfd642b4d53b7e30509e348eefc976f83985d9d5
snapyy <210510113165@paruluniversity.ac.in>
2024-05-07 20:27:03 +0530

    Update 7_strings.js

--- practice-1/javascript-starter-file/7_strings.js
+++ practice-1/javascript-starter-file/7_strings.js
@@ -18,9 +18,128 @@
 // let strMap = strArr.map((currElement, index) => `${currElement}- ${index}`);
 // console.log(strMap);
 
-let text = "Hello JavaScript, welcome to our world best JavaScript course";
+// let text = "Hello JavaScript, welcome to our world best JavaScript course";
 
-let index = text.lastIndexOf("JavaScript");
-let dk = text.lastIndexOf("JavaScript", 40);
-console.log(index);
-console.log(dk);
+// let index = text.lastIndexOf("JavaScript");
+// let dk = text.lastIndexOf("JavaScript", 40);
+// console.log(index);
+// console.log(dk);
+
+//* Using search() method
+// let text = "Hello JavaScript, welcome to our world best JavaScript course";
+// let result = text.search(/Javascript/i);
+// console.log(result);
+
+//* Using match() method
+
+// let text = "Hello JavaScript, welcome to our world best JavaScript course";
+// let result = text.match("JavaScript");
+// let result = text.match(/Javascript/gi);
+// console.log(result);
+
+//* Using matchAll() method
+
+// let text = "Hello JavaScript, welcome to our world best JavaScript course";
+
+// let result = text.matchAll("Javascript");
+// let result = text.matchAll("JavaScript");
+
+//* These are the ways to print matchAll() method output
+// console.log(...result);
+
+// for (let item of result) {
+//   console.log(item[0]);
+// }
+
+// for (let index of result) {
+//   console.log(index.index);
+// }
+
+// for (let { index } of result) {
+//   console.log(index);
+// }
+
+//* Using includes() method
+
+// let text = "Hello JavaScript, welcome to our world best JavaScript course";
+
+// let result = text.includes("Java");
+// let result = text.includes("java");
+
+// console.log(result);
+
+//* Using startsWith() method
+
+// let text = "Hello JavaScript, welcome to our world best JavaScript course";
+
+// let result = text.startsWith("Welcome");
+// let result = text.startsWith("Hello");
+
+// console.log(result);
+
+//* Using endsWith() method
+
+// let text = "Hello JavaScript, welcome to our world best JavaScript course";
+
+// let result = text.endsWith("course");
+// console.log(result);
+
+//* Extracting String Parts
+
+// let text = "Hello JavaScript, welcome to our world best JavaScript course";
+
+// let result = text.slice(6, 16);
+// console.log(result);
+
+//* Using substring() method
+
+// let text = "Hello JavaScript, welcome to our world best JavaScript course";
+
+// let result = text.substring(6, 16);
+// let result = text.substring(-6);
+// console.log(result);
+
+//* Using charAt() method
+
+// let text = "Hello JavaScript, welcome to our world best JavaScript course";
+
+// let result = text.charAt(6);
+// let result = text.charAt(-6); // outcome : empty string
+
+// console.log(result);
+
+//* Using charCodeAt() method
+
+// let text = "Hello JavaScript, welcome to our world best JavaScript course";
+
+// let result = text.charCodeAt(6);
+// let result = text.charCodeAt(-6);
+// console.log(result);
+
+//* Using at() method
+
+// let text = "Hello JavaScript, welcome to our world best JavaScript course";
+// let result = text.at(-6);
+// console.log(result);
+
+//* Replacing String Content :-
+//* Using replace() method
+
+// let text = "Hello JavaScript, welcome to our world best JavaScript course";
+// let result = text.replace(/javascript /gi, "Baki Hanma ");
+// console.log(result);
+
+//* Using trim() method
+
+// const str = "   Hello,World!      ";
+// console.log(str.length);
+// let trimStr = str.trim();
+// console.log(trimStr.length);
+
+//* Using split() method
+
+// const str = "apple,orange,save,banana";
+// let result = str.split(",");
+// let result = str.split(",").reverse().join();
+
+// console.log(result);
